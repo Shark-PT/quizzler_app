6010b5d66e47bb637e96d05ef344d30d82365d0e
Shark-PT <jccreis@hotmail.com>
2023-12-12 12:51:10 +0000

    Modulo quizBrain actualizado

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self,quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,6 +16,7 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 125,
+                                                     width=280,
                                                      text="question",
                                                      fill=THEME_COLOR,
                                                      font=("Arial", 20, "italic"))
@@ -26,5 +29,10 @@ class QuizInterface:
         false_img = PhotoImage(file="./images/false.png")
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+       q_text = self.quiz.next_question()
+       self.canvas.itemconfig(self.question_text, text=q_text)
