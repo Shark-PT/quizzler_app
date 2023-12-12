2b8fb014953301270feb7344154181c51bfbdf6e
Shark-PT <jccreis@hotmail.com>
2023-12-12 13:24:47 +0000

    feedback para o user no ui

--- ui.py
+++ ui.py
@@ -23,16 +23,32 @@ class QuizInterface:
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_img = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button = Button(image=true_img, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
         false_img = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button = Button(image=false_img, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
-       q_text = self.quiz.next_question()
-       self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
